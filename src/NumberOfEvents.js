2ef311d81223791653a6c6009bd38ea224256e88
PriyankaNirgude29 <piu.pawar@gmail.com>
2022-06-07 19:42:36 +0200

    updated NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
+import { InputGroup, FormControl, Col } from 'react-bootstrap';
 
 class NumberOfEvents extends Component {
+  state = { 
+    numberOfEvents : 12
+  }
 
+  changeNumOfEvents = (e) => {
+    let newValue = parseInt(e.target.value);
+    if ((newValue > 33) || (newValue < 1)) {
+      this.setState({
+        numberOfEvents: newValue,
+        infoText: 'Please set a number between 1 and 32',
+      })
+    } else {
+      this.setState({
+        numberOfEvents: newValue,
+        infoText: ' ',
+      })
+    }
+        this.props.updateEvents(undefined, newValue);
+  }
 
   render() {
     return (
-      <div className="NumberOfEvents">
-        <input 
-          className="number-of-events"
-          type="number"
-          value={this.props.numberOfEvents}>
-        </input>
-      </div>
+      <>
+      <Col>
+      {/* <div className="numberOfEvents">Events to show:</div> */}
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">Number of events to show</InputGroup.Text>
+        <FormControl
+          placeholder="Number between 1 and 32"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+          className="number-of-events" 
+            type="number" 
+            onChange={this.changeNumOfEvents}
+            value={this.state.numberOfEvents}
+        />
+      </InputGroup>
+      </Col>
+      </>
     );
   }
 }
