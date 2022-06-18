d61218bff5c88758735dbb1d85b8c58a73b63f93
PriyankaNirgude29 <piu.pawar@gmail.com>
2022-06-18 05:49:57 +0200

    Integrated Atatus

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('3c8eb5fa8934455b94d783dd4390ceef').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-//React 18
-// const rootElement = document.getElementById("root");
-// const root = createRoot(rootElement);
-
 root.render(
   <React.StrictMode>
         <App />
@@ -25,3 +24,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
