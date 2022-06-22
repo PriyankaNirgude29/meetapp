90761798d6ad58165bb9b4346cf55f73d21a84ff
PriyankaNirgude29 <piu.pawar@gmail.com>
2022-06-22 11:14:29 +0200

    done some coding changes

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -48,7 +48,8 @@ class CitySearch extends Component {
       <Col>
       {/* <div className="CitySearch">Choose a city to see the events having place there:</div> */}
       
-      <InputGroup className="mb-3">Meet App
+      <InputGroup className="mb-3">
+        <h4>Meet App</h4><br></br>
         <InputGroup.Text id="basic-addon1">Choose your nearest city</InputGroup.Text>
         <FormControl
           placeholder="Type the name of the city you are looking for"
